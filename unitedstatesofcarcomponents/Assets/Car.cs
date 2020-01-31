860909cc2c88f09385e042fffc108f1c8215f8e7
Erik <e.naaru@hotmail.com>
2020-01-31 23:45:59 +0100

    Implement rudimentary driveable Car prefab

---
+++ unitedstatesofcarcomponents/Assets/Car.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour {
+    public float acceleration;
+    public float velocityDecay;
+    public float rotationAcceleration;
+    public float rotationVelocityDecay;
+
+    private Vector3 velocity;
+    private float rotationVelocity = 0; //Negative right, positive right.
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        UpdateInput();
+        UpdateMovement();
+    }
+
+    private void UpdateMovement() {
+        transform.Rotate(0, rotationVelocity, 0);
+        transform.position += velocity;
+
+        velocity *= velocityDecay;
+        rotationVelocity *= rotationVelocityDecay;
+    }
+
+    private void UpdateInput() {
+        velocity += transform.forward * acceleration * Input.GetAxis("Accelerate");
+        velocity -= transform.forward * acceleration * Input.GetAxis("Reverse");
+        
+        rotationVelocity += Input.GetAxis("Horizontal") * rotationAcceleration * Vector3.Project(velocity, transform.forward).normalized.magnitude;
+    }
+}
