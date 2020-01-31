6b9aca1f7b21699d5c6f94c868823d09bc9a12d5
Fendse <46252070+Fendse@users.noreply.github.com>
2020-01-31 22:33:44 +0100

    Non-functional car parts, to allow other parts of game to refer to the class (tho they cant do anything w/ it yet)

---
+++ unitedstatesofcarcomponents/Assets/Parts/CarPart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CarPart : MonoBehaviour
+{
+    private void Start() {
+        currentHealth = maxHealth;
+    }
+
+    private int maxHealth;
+
+    public int currentHealth;
+
+    public int healthDecay;
+
+    private void Update() {
+        currentHealth -= (int) (Time.deltaTime * healthDecay);
+
+        if (currentHealth <= 0) {
+            // Destroy this part
+        }
+
+
+    }
+
+    void OnEquip() {
+        // Default implementation
+    }
+
+}
