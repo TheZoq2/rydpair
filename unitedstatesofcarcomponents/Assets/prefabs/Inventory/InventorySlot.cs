affb5b120d66502a8feca92317c20c508feff271
Axel Nordanskog <axeno840@student.liu.se>
2020-02-01 00:10:29 +0100

    Add inventory which slots show circles when filled

---
+++ unitedstatesofcarcomponents/Assets/prefabs/Inventory/InventorySlot.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+	private GameObject item;
+	private Image itemSprite;
+
+	private void Start()
+	{
+		itemSprite = transform.Find("ItemSprite").GetComponent<Image>();
+		ForceSet(null);
+
+		// Dummy test
+		if (gameObject.name == "InventorySlot (1)" || gameObject.name == "InventorySlot (5)")
+		{
+			ForceSet(gameObject);
+		}
+	}
+
+	private void ForceSet(GameObject new_item)
+	{
+		item = new_item;
+		itemSprite.enabled = item != null;
+	}
+
+	/**
+	 * Returns false if already occupied.
+	 * Otherwise sets item at posiiton and returns true.
+	 */
+	public bool TrySet(GameObject new_item)
+	{
+		if (item == null)
+		{
+			ForceSet(new_item);
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Returns false if isn't occupied.
+	 * Otherwise removes item and returns true.
+	 */
+	public bool TryClear()
+	{
+		if (item != null)
+		{
+			ForceSet(null);
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
