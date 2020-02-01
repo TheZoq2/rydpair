e627df788863cf9b647acf0a7f187a36d09c86c9
Fendse <46252070+Fendse@users.noreply.github.com>
2020-02-01 22:45:23 +0100

    Cleaned up that part-creator-function.

--- unitedstatesofcarcomponents/Assets/CarPartFactory.cs
+++ unitedstatesofcarcomponents/Assets/CarPartFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CarPartFactory : MonoBehaviour {
@@ -18,89 +19,11 @@ public class CarPartFactory : MonoBehaviour {
 
     // YOU MADE ME TO THIS!
     public CarPart Create(PartTypes type, Manufacturers manufacturer) {
-        switch (type) {
-            case PartTypes.BRAKES:
-                switch (manufacturer) {
-                    case Manufacturers.NII_SAN:
-                        return CreateNiiSanBrakes();
-                    case Manufacturers.SM:
-                        return CreateSMBrakes();
-                    case Manufacturers.VOLVIMUS:
-                        return CreateVolvimusBrakes();
-                    default:
-                        Application.Quit(-1);
-                        return null;
-                }
-            case PartTypes.ENGINE:
-                switch (manufacturer) {
-                    case Manufacturers.NII_SAN:
-                        return CreateNiiSanEngine();
-                    case Manufacturers.SM:
-                        return CreateSMEngine();
-                    case Manufacturers.VOLVIMUS:
-                        return CreateVolvimusEngine();
-                    default:
-                        Application.Quit(-1);
-                        return null;
-                }
-            case PartTypes.EXHAUST_SYSTEM:
-                switch (manufacturer) {
-                    case Manufacturers.NII_SAN:
-                        return CreateNiiSanExhaust();
-                    case Manufacturers.SM:
-                        return CreateSMExhaust();
-                    case Manufacturers.VOLVIMUS:
-                        return CreateVolvimusExhaust();
-                    default:
-                        Application.Quit(-1);
-                        return null;
-                }
-            case PartTypes.GEAR_BOX:
-                switch (manufacturer) {
-                    case Manufacturers.NII_SAN:
-                        return CreateNiiSanGearBox();
-                    case Manufacturers.SM:
-                        return CreateSMGearBox();
-                    case Manufacturers.VOLVIMUS:
-                        return CreateVolvimusGearBox();
-                    default:
-                        Application.Quit(-1);
-                        return null;
-                }
-            case PartTypes.STEERING_WHEEL:
-                switch (manufacturer) {
-                    case Manufacturers.NII_SAN:
-                        return CreateNiiSanSteeringWheel();
-                    case Manufacturers.SM:
-                        return CreateSMSteeringWheel();
-                    case Manufacturers.VOLVIMUS:
-                        return CreateVolvimusSteeringWheel();
-                    default:
-                        Application.Quit(-1);
-                        return null;
-                }
-            case PartTypes.WHEELS:
-                switch (manufacturer) {
-                    case Manufacturers.NII_SAN:
-                        return CreateNiiSanWheels();
-                    case Manufacturers.SM:
-                        return CreateSMWheels();
-                    case Manufacturers.VOLVIMUS:
-                        return CreateVolvimusWheels();
-                    default:
-                        Application.Quit(-1);
-                        return null;
-                }
+        Dictionary<PartTypes, Dictionary<Manufacturers, Action<Car>>> callbacks = new Dictionary<PartTypes, Dictionary<Manufacturers, Action<Car>>>();
+        foreach (PartTypes partType in Enum.GetValues(typeof(PartTypes))) {
+            callbacks[partType] = new Dictionary<Manufacturers, Action<Car>>();
         }
-        return null;
-    }
-
-    /// <summary>
-    /// Engine that has terrible fuel drain if not using a NII_SAN Gearbox and/or terrible acceleration if not using a NII_SAN Steering wheel.
-    /// </summary>
-    /// <returns></returns>
-    public CarPart CreateNiiSanEngine() {
-        return Create(PartTypes.ENGINE, Manufacturers.NII_SAN, 
+        callbacks[PartTypes.ENGINE][Manufacturers.NII_SAN] =
             c => {
                 if(c.equippedParts[PartTypes.GEAR_BOX].manufacturer != Manufacturers.NII_SAN) {
                     c.fuelDrain *= 10;
@@ -109,127 +32,78 @@ public class CarPartFactory : MonoBehaviour {
                 if (c.equippedParts[PartTypes.STEERING_WHEEL].manufacturer != Manufacturers.NII_SAN) {
                     c.acceleration /= 3;
                 }
-            });
-    }
-
-    public CarPart CreateVolvimusEngine() {
-        return Create(PartTypes.ENGINE, Manufacturers.VOLVIMUS, 
+            };
+        callbacks[PartTypes.ENGINE][Manufacturers.VOLVIMUS] =
             c => {
                 if(c.equippedParts[PartTypes.EXHAUST_SYSTEM].manufacturer == Manufacturers.SM) {
                     c.engineSmoke.Play();
                 }
-            });
-    }
-
-    public CarPart CreateSMEngine() {
-        return Create(PartTypes.ENGINE, Manufacturers.SM,
+            };
+        callbacks[PartTypes.ENGINE][Manufacturers.SM] =
             c => {
 
-            });
-    }
-
-    public CarPart CreateNiiSanWheels() {
-        return Create(PartTypes.WHEELS, Manufacturers.NII_SAN,
+            };
+        callbacks[PartTypes.WHEELS][Manufacturers.NII_SAN] =
             c => {
 
-            });
-    }
-
-    public CarPart CreateVolvimusWheels() {
-        return Create(PartTypes.WHEELS, Manufacturers.VOLVIMUS,
+            };
+        callbacks[PartTypes.WHEELS][Manufacturers.VOLVIMUS] =
             c => {
 
-            });
-    }
-
-    public CarPart CreateSMWheels() {
-        return Create(PartTypes.WHEELS, Manufacturers.SM,
+            };
+        callbacks[PartTypes.WHEELS][Manufacturers.SM] =
             c => {
 
-            });
-    }
-
-    public CarPart CreateNiiSanGearBox() {
-        return Create(PartTypes.GEAR_BOX, Manufacturers.NII_SAN, 
+            };
+        callbacks[PartTypes.GEAR_BOX][Manufacturers.NII_SAN] =
             c => {
                 c.maxFuel = 5;
-            });
-    }
-
-    public CarPart CreateVolvimusGearBox() {
-        return Create(PartTypes.GEAR_BOX, Manufacturers.VOLVIMUS,
+            };
+        callbacks[PartTypes.GEAR_BOX][Manufacturers.VOLVIMUS] =
             c => {
                 c.maxFuel = 5;
-            });
-    }
-
-    public CarPart CreateSMGearBox() {
-        return Create(PartTypes.GEAR_BOX, Manufacturers.SM,
+            };
+        callbacks[PartTypes.GEAR_BOX][Manufacturers.SM] =
             c => {
                 c.maxFuel = 5;
-            });
-    }
-
-    public CarPart CreateNiiSanExhaust() {
-        return Create(PartTypes.EXHAUST_SYSTEM, Manufacturers.NII_SAN,
+            };
+        callbacks[PartTypes.EXHAUST_SYSTEM][Manufacturers.NII_SAN] =
             c => {
                 c.maxFuel = 5;
-            });
-    }
-
-    public CarPart CreateVolvimusExhaust() {
-        return Create(PartTypes.EXHAUST_SYSTEM, Manufacturers.VOLVIMUS,
+            };
+        callbacks[PartTypes.EXHAUST_SYSTEM][Manufacturers.VOLVIMUS] =
             c => {
                 c.maxFuel = 5;
-            });
-    }
-
-    public CarPart CreateSMExhaust() {
-        return Create(PartTypes.EXHAUST_SYSTEM, Manufacturers.SM,
+            };
+        callbacks[PartTypes.EXHAUST_SYSTEM][Manufacturers.SM] =
             c => {
                 c.maxFuel = 5;
-            });
-    }
-
-    public CarPart CreateNiiSanBrakes() {
-        return Create(PartTypes.BRAKES, Manufacturers.NII_SAN,
+            };
+        callbacks[PartTypes.BRAKES][Manufacturers.NII_SAN] =
             c => {
                 c.maxFuel = 5;
-            });
-    }
-
-    public CarPart CreateVolvimusBrakes() {
-        return Create(PartTypes.BRAKES, Manufacturers.VOLVIMUS,
+            };
+        callbacks[PartTypes.BRAKES][Manufacturers.VOLVIMUS] =
             c => {
                 c.maxFuel = 5;
-            });
-    }
-
-    public CarPart CreateSMBrakes() {
-        return Create(PartTypes.BRAKES, Manufacturers.SM,
+            };
+        callbacks[PartTypes.BRAKES][Manufacturers.SM] =
             c => {
                 c.maxFuel = 5;
-            });
-    }
-
-    public CarPart CreateNiiSanSteeringWheel() {
-        return Create(PartTypes.STEERING_WHEEL, Manufacturers.NII_SAN,
+            };
+        callbacks[PartTypes.STEERING_WHEEL][Manufacturers.NII_SAN] =
             c => {
                 c.maxFuel = 5;
-            });
-    }
-
-    public CarPart CreateVolvimusSteeringWheel() {
-        return Create(PartTypes.STEERING_WHEEL, Manufacturers.VOLVIMUS,
+            };
+        callbacks[PartTypes.STEERING_WHEEL][Manufacturers.VOLVIMUS] =
             c => {
                 c.maxFuel = 5;
-            });
-    }
-
-    public CarPart CreateSMSteeringWheel() {
-        return Create(PartTypes.STEERING_WHEEL, Manufacturers.SM, 
+            };
+        callbacks[PartTypes.STEERING_WHEEL][Manufacturers.SM] =
             c => {
                 c.maxFuel = 5;
-            });
+            };
+
+        return Create(type, manufacturer, callbacks[type][manufacturer]);
     }
 }
