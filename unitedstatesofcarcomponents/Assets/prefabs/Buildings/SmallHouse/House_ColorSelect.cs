82c5fcc2ad27767b2e6c764a0f3e2e6aa8f19924
Mattias <mattias.jenvald@gmail.com>
2020-02-01 12:27:02 +0100

    Small house prefab

---
+++ unitedstatesofcarcomponents/Assets/prefabs/Buildings/SmallHouse/House_ColorSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House_ColorSelect : MonoBehaviour
+{
+    [Header("ColorParent")]
+    public List<Sprite> sprites;
+
+    [Header("Material")]
+    public Material houseMat;
+
+    int randomIndex;
+    
+    private void OnEnable()
+    {
+        ChangeTexture();
+    }
+
+    void ChangeTexture()
+    {
+        randomIndex = Random.Range(0, sprites.Count);
+
+        houseMat.SetTexture("_MainTex", sprites[randomIndex].texture);
+    }
+}
