40b75cf29ae4a163c8ab170bb767aac84c035f44
Erik <e.naaru@hotmail.com>
2020-02-01 13:37:29 +0100

    Add methods for adding and removing car parts

---
+++ unitedstatesofcarcomponents/Assets/Parts/PartTypes.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PartTypes { ENGINE, WHEELS, GEAR_BOX, STEERING_WHEEL, BRAKES, EXHAUST_SYSTEM }
--- unitedstatesofcarcomponents/Assets/prefabs/Car/Car.cs
+++ unitedstatesofcarcomponents/Assets/prefabs/Car/Car.cs
@@ -7,11 +7,23 @@ public class Car : MonoBehaviour {
     public float rotationSpeed;
     public float turnMultiplier;
 
-    private Vector3 velocity;
+    public float maxFuel;
+    public float fuelDrain;
 
-    // Start is called before the first frame update
-    void Start() {
-        
+    //Equipped car parts
+    public CarPart engine;
+    public CarPart wheels;
+    public CarPart gearBox;
+    public CarPart steeringWheel;
+    public CarPart brakes;
+    public CarPart exhaustSystem;
+
+    private Vector3 velocity;
+    
+    private float fuel;
+
+    private void Start() {
+        fuel = maxFuel;
     }
 
     // Update is called once per frame
@@ -20,16 +32,79 @@ public class Car : MonoBehaviour {
         UpdateMovement();
     }
 
-    public void UpdateComponentEffects() {
+    public void RemovePart(PartTypes type) {
+        switch (type) {
+            //TODO: Instead of null, have default broken part?
+            case PartTypes.ENGINE:
+                engine = null;
+                    break;
+            case PartTypes.WHEELS:
+                wheels = null;
+                break;
+            case PartTypes.GEAR_BOX:
+                wheels = null;
+                break;
+            case PartTypes.STEERING_WHEEL:
+                steeringWheel = null;
+                break;
+            case PartTypes.BRAKES:
+                brakes = null;
+                break;
+            case PartTypes.EXHAUST_SYSTEM:
+                exhaustSystem = null;
+                break;
+            default:
+                break;
+        }
+        UpdateComponentEffects();
+    }
 
+    public void AddPart(PartTypes type, CarPart newPart) {
+        switch (type) {
+            case PartTypes.ENGINE:
+                engine = newPart;
+                break;
+            case PartTypes.WHEELS:
+                wheels = newPart;
+                break;
+            case PartTypes.GEAR_BOX:
+                wheels = newPart;
+                break;
+            case PartTypes.STEERING_WHEEL:
+                steeringWheel = newPart;
+                break;
+            case PartTypes.BRAKES:
+                brakes = newPart;
+                break;
+            case PartTypes.EXHAUST_SYSTEM:
+                exhaustSystem = newPart;
+                break;
+            default:
+                break;
+        }
+        UpdateComponentEffects();
+    }
+
+    public void UpdateComponentEffects() {
+        //TODO: Implement CarPart method for affecting car
+        //engine.
+        //wheels.
+        //gearbox.
+        //steeringWheel.
+        //brakes.
+        //exhaustSystem.
     }
 
     private void UpdateMovement() {
-        transform.position += velocity * Time.deltaTime;
+        if(fuel > 0) {
+            transform.position += velocity * Time.deltaTime;
 
-        velocity *= velocityDecay;
-        if(Input.GetAxis("Accelerate") != 0 && Input.GetAxis("Reverse") != 0 && velocity.magnitude < minimumSpeed) {
-            velocity = Vector3.zero;
+            velocity *= velocityDecay;
+            if(Input.GetAxis("Accelerate") != 0 && Input.GetAxis("Reverse") != 0 && velocity.magnitude < minimumSpeed) {
+                velocity = Vector3.zero;
+            }
+
+            fuel -= fuelDrain;
         }
     }
 
