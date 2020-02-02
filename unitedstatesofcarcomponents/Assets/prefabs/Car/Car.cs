b02c8e9f14ceccb215ce98fc315f09aaaedeee4a
Axel Nordanskog <axeno840@student.liu.se>
2020-02-02 11:00:41 +0100

    Cripple car on missing parts

--- unitedstatesofcarcomponents/Assets/prefabs/Car/Car.cs
+++ unitedstatesofcarcomponents/Assets/prefabs/Car/Car.cs
@@ -8,24 +8,36 @@ public class Car : MonoBehaviour {
     private CarPartFactory carPartFactory;
     public GameObject steeringWheel;
     public GameObject spedometerNeedle;
-    public float steeringWheelMultiplier;
 
-    public float defaultAcceleration;
-    public float acceleration;
-    public float defaultMaxFuel;
-    public float maxFuel;
-    public float defaultFuelDrain;
-    public float fuelDrain;
-    public float defaultMaxVelocity;
-    public float maxVelocity;
-    public float defaultVelocityDecay;
-    public float velocityDecay;
+	[HideInInspector]
+	public float steeringWheelMultiplier;
+	[HideInInspector]
+	public float acceleration;
+	[HideInInspector]
+	public float maxFuel;
+	[HideInInspector]
+	public float fuelDrain;
+	[HideInInspector]
+	public float maxVelocity;
+	[HideInInspector]
+	public float velocityDecay;
+	[HideInInspector]
+	public float wheelDistance;
+	[HideInInspector]
+	public float steeringSpeed;
 
-    public float wheelDistance;
-    public float steeringSpeed;
+	public float defaultSteeringWheelMultiplier;
+	public float defaultAcceleration;
+	public float defaultMaxFuel;
+	public float defaultFuelDrain;
+	public float defaultMaxVelocity;
+	public float defaultVelocityDecay;
+	public float defaultWheelDistance;
+	public float defaultSteeringSpeed;
 
-    private string destinationAddress = "";
-    public int score = 0;
+	private string destinationAddress = "";
+	[HideInInspector]
+	public int score = 0;
 
     //Equipped car parts
     public Dictionary<PartTypes, CarPart> equippedParts = new Dictionary<PartTypes, CarPart>();
@@ -73,10 +85,12 @@ public class Car : MonoBehaviour {
         }
     }
 
-    public CarPart RemovePart(PartTypes type) {
-        CarPart removedPart = equippedParts[type];
+    public CarPart RemovePart(PartTypes type)
+	{
+		CarPart removedPart = equippedParts[type];
         equippedParts[type] = null;
-        return removedPart;
+		UpdateComponentEffects();
+		return removedPart;
     }
 
 
@@ -88,18 +102,55 @@ public class Car : MonoBehaviour {
 
     public void UpdateComponentEffects() {
         ResetStats();
-        foreach (CarPart part in equippedParts.Values) {
+		// Missing parts effects
+		foreach (PartTypes type in equippedParts.Keys)
+		{
+			if (equippedParts[type] == null)
+			{
+				switch (type)
+				{
+					case PartTypes.BRAKES:
+						// TODO
+						break;
+					case PartTypes.ENGINE:
+						acceleration *= 0.1f;
+						maxVelocity *= 0.1f;
+						break;
+					case PartTypes.EXHAUST_SYSTEM:
+						engineSmoke.Play();
+						break;
+					case PartTypes.GEAR_BOX:
+						acceleration *= 0.1f;
+						maxVelocity *= 0.1f;
+						break;
+					case PartTypes.STEERING_WHEEL:
+						steeringSpeed *= 0.1f;
+						break;
+					case PartTypes.WHEELS:
+						acceleration *= 0.5f;
+						maxVelocity *= 0.5f;
+						break;
+				}
+			}
+		}
+		// Part-specific effects
+		foreach (CarPart part in equippedParts.Values) {
             part?.UpdateEffect(this);
         }
     }
 
     private void ResetStats() {
-        maxFuel = defaultMaxFuel;
-        fuelDrain = defaultFuelDrain;
-        maxVelocity = defaultMaxVelocity;
+		steeringWheelMultiplier = defaultSteeringWheelMultiplier;
+		acceleration = defaultAcceleration;
+		maxFuel = defaultMaxFuel;
+		fuelDrain = defaultFuelDrain;
+		maxVelocity = defaultMaxVelocity;
+		velocityDecay = defaultVelocityDecay;
+		wheelDistance = defaultWheelDistance;
+		steeringSpeed = defaultSteeringSpeed;
 
-        // Clear smoke colour
-        ParticleSystem.Particle[] particleArray = new ParticleSystem.Particle[1];
+	// Clear smoke colour
+	ParticleSystem.Particle[] particleArray = new ParticleSystem.Particle[1];
         if (engineSmoke.GetParticles(particleArray) > 0) {
             particleArray[0].startColor = new Color(0, 0, 0);
         }
